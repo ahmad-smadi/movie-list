fec032d7d5c8da6254aeeba7ebef6c077bcffb9f
ahmad smadi <84705476+ahmad-smadi@users.noreply.github.com>
2021-07-06 12:13:54 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ let pic = [
 
 function Movies (name , image , release){
   this.name = name; 
-  this.image = image;
+  this.image =  `./img/${image}.png`;
   this.release = release ;
   Movies.allMovies.push(this)
 };
@@ -120,20 +120,30 @@ function creatTable (){
   }
 }
 function render () {
-  let tbody = document.querySelector('tbody')
-
-  let tr = document.createElement('tr');
-  tbody.appendChild(tr)
-
- let td1 = document.createElement('td');
- td1.textContent = this.name; 
- tr.appendChild(td1)
-
- let td2 = document.createElement('td');
- td2.textContent = this.image; 
- tr.appendChild(td2)
-
- let td3 = document.createElement('td');
- td3.textContent = this.release; 
- tr.appendChild(td3)
+   list.innerHTML = ""
+    let table = document.createElement('table');
+    // createTableHeader(table);
+    let tbody = document.createElement('tbody');
+    for (let i=0 ; i < arr.length; i++) {
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+        td.innerText = "X";
+        td.className = i;
+        tr.appendChild(td);
+        let img = document.createElement('img')
+        img.src = arr[i].image;
+        td = document.createElement('td');
+        td.appendChild(img);
+        tr.appendChild(td);
+        td = document.createElement('td');
+        td.innerText = arr[i].name;
+        tr.appendChild(td);
+        td = document.createElement('td');
+        td.innerText = arr[i].release;
+        tr.appendChild(td);
+        tbody.appendChild(tr);
+    }
+    table.appendChild(tbody);
+    list.appendChild(table);
+    Movies.prototype.setLocalStorge();
 }
